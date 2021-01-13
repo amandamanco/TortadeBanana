32d54d6a8504c96af438919c04a30df3820ac1d5
amanda.manco <amand113@hotmail.com>
2021-01-13 18:41:40 -0300

    Ajuste bug hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Center (
-        
-          child: Text("HELLO WORLD"),
+    NewWidget(),
+    );
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key key,
+  }) : super (key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container (
+        color: Colors.pink,
+        child: Center(
+          child: Text(
+            "HELLO WORLD", 
+            style: Theme.of(context).primaryTextTheme.bodyText1,
           ),
         ),
+      ),
     );
+  }
 }
